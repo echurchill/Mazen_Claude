702f5e03318493bd3cf9f70a9ca2e337ce9e78d8
echurchill <echurchill@users.noreply.github.com>
2026-06-16 12:40:06 -0500

    Initial Commit

---
+++ Mazen_Claude macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Mazen_Claude macOS
+//
+//  Created by Eddie Churchill on 6/16/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
